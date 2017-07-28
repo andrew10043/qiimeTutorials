d87f51354a966ccbfc943b90bab09ae85ffd3f89
Ben Andrew <benjamin.andrew@duke.edu>
2017-07-28 11:55:52 -0400

    Updated full run script, added 06.1 module.

--- illuminaTutorial/illuminaTutorial.sh
+++ illuminaTutorial/illuminaTutorial.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Runs full analysis, beginning with data download and import
+
 source 00_downloadData.sh
 
 python 01_importData.py
@@ -11,3 +13,13 @@ source 03.1_demultiplexQualityFilter.sh
 python 03.2_demultiplexQualityFilter.py
 
 source 03.3_demultiplexQualityFilter.sh
+
+source 04_otuPicking.sh
+
+source 05_summaryStatistics.sh
+
+source 06.1_diversityAnalysis.sh
+
+source 06.2_diversityAnalysis.sh
+
+source 06.3_diversityAnalysis.sh
